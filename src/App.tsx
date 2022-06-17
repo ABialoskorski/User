3f23c3ea79af85140913dca52b10817c8c1f9c1b
Artur Białoskórski <bialoskorski.artur@gmail.com>
2022-06-17 13:58:03 +0200

    description

--- src/App.tsx
+++ src/App.tsx
@@ -4,10 +4,12 @@ import { Header } from './common/components/Navigation/Header/Header';
 import { Footer } from './common/components/Footer/Footer';
 import { UserContextProvider } from './UserContext/UserContext';
 import { SideMenu } from './common/components/Navigation/SideMenu/SideMenu';
+import { ScrollToTop } from './common/components/ScrollToTop/ScrollToTop';
 import { AppContainer } from './App.styles';
 
 export const App: FC = () => (
   <div className='App'>
+    <ScrollToTop />
     <UserContextProvider>
       <Header />
       <AppContainer>
